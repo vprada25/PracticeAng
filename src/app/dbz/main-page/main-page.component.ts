831c50bb9dad5daabd026094c28856e2976907cd
vprada25 <v.prada@udla.edu.co>
2021-04-07 13:17:19 -0500

    feat(forms): ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  name: string;
+  can: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  nuevo: Personaje = {
+    name: 'trunks',
+    can: 14000
+  }
 
-  ngOnInit(): void {
+  addfighter() {
+    console.log(this.nuevo)
   }
 
 }
